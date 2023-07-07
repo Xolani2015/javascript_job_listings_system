c583749c2549dd05c46cf5b395f8a1a943f4b76a
Xolani <xr.mkhwanazi.20@gmail.com>
2023-07-07 10:31:00 -0700

    feat: completed saving of jobs and displaying of saved jobs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
@@ -13,11 +14,46 @@ app.use(cors());
 //     database: "joblistingsdb"
 // })
 
+function callJobsApi() { 
+    let url = "https://arbeitnow.com/api/job-board-api";
+    fetch(url).then((res)=> { 
+        res.json().then((res1)=> { 
+            console.log(res1);
+        })
+    })
+}
+
 const db =  mysql.createConnection({
     host: "localhost",
     user: "root", 
     password: "", 
     database: "joblistingsdb"
+})
+
+app.get("/", (req, res) => {
+  
+    const sql = "SELECT * FROM jobs";
+    db.query(sql, (err, data) => { 
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+
+})
+
+app.get("/getAllJobs", (req, res) => {
+//   var api_data = callJobsApi();
+//   const jobData = JSON.parse(api_data);
+//   // Display the job titles
+// jobData.forEach((job) => {
+//     console.log(job.title);
+//   });
+  
+//   // Display the company names
+//   jobData.forEach((job) => {
+//     console.log(job.company_name);
+//   });
+var jobData = {'1' : "Job"};
+        return jobData;
 })
 
 app.get("/", (req, res) => {
